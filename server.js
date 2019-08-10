fdff2952da6b387c20478bc658b9ba6b815749bc
JonghyunJung <jh0116j@gmail.com>
2019-08-10 20:25:05 +0900

    'tetst'

--- server.js
+++ server.js
@@ -8,21 +8,10 @@ var server = http.createServer(function(request,response){
 
   // 1. 요청된 자원이 /hello 이면
   if(resource == '/index'){
-    f_index();
+    f_index(request,response);
   }// 1. 요청된 자원이 /hello 이면
   else if(resource == '/item'){
-    // 2. hello.html 파일을 읽은 후
-    fs.readFile('item.html', 'utf-8', function(error, data) {
-      // 2.1 읽으면서 오류가 발생하면 오류의 내용을
-      if(error){
-        response.writeHead(500, {'Content-Type':'text/html'});
-        response.end('500 Internal Server Error : '+error);
-      // 2.2 아무런 오류가 없이 정상적으로 읽기가 완료되면 파일의 내용을 클라이언트에 전달
-      }else{
-        response.writeHead(200, {'Content-Type':'text/html'});
-        response.end(data);
-      }
-    });
+    f_item(request,response);
   }
   else{
     response.writeHead(404, {'Content-Type':'text/html'});
@@ -30,7 +19,7 @@ var server = http.createServer(function(request,response){
   }
 
 });
-function f_index(){
+function f_index(request,response){
   // 2. hello.html 파일을 읽은 후
   fs.readFile('index.html', 'utf-8', function(error, data) {
     // 2.1 읽으면서 오류가 발생하면 오류의 내용을
@@ -44,6 +33,20 @@ function f_index(){
     }
   });
 };
+function f_item(request,response){
+      // 2. hello.html 파일을 읽은 후
+    fs.readFile('item.html', 'utf-8', function(error, data) {
+      // 2.1 읽으면서 오류가 발생하면 오류의 내용을
+      if(error){
+        response.writeHead(500, {'Content-Type':'text/html'});
+        response.end('500 Internal Server Error : '+error);
+      // 2.2 아무런 오류가 없이 정상적으로 읽기가 완료되면 파일의 내용을 클라이언트에 전달
+      }else{
+        response.writeHead(200, {'Content-Type':'text/html'});
+        response.end(data);
+      }
+    });
+}
 server.listen(8899, function(){
     console.log('Server is running...');
 });
